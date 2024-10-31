84e1bdd01f68171ae63c03d4af9fc398bfb7e98e
zkco <qkrentks456@gmail.com>
2024-10-31 17:16:23 +0900

    [Add] ItemData

---
+++ Assets/Scripts/Item/Data/ItemData.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ItemType
+{
+    Equipable,
+    Consumable,
+    Readable,
+}
+
+public enum ConsumeType
+{
+    Temporaly,
+    Durably,
+}
+public enum TargetStat
+{
+    Battery,
+    Stamina
+}
+
+[Serializable]
+public class ItemEffect
+{
+    public TargetStat TargetStat;
+    public float Value;
+}
+
+[CreateAssetMenu(fileName = "Item", menuName = "New Item", order = 0)]
+public class ItemData : ScriptableObject
+{
+    public ItemType ItemType;
+
+    [Header("Comsume")]
+    public ConsumeType ConsumeType;
+    public ItemEffect[] Effect;
+}
+
+

cb6ada224403ced367e593067e0887fb4a8ef2c4
zkco <qkrentks456@gmail.com>
2024-11-07 01:07:19 +0900

    Update AuditoryDoor.cs

--- Assets/Scripts/Data/Animated/AuditoryDoor.cs
+++ Assets/Scripts/Data/Animated/AuditoryDoor.cs
@@ -8,10 +8,10 @@ public class AuditoryDoor : Door
 {
 	public override void Temp()
 	{
-		if (QuestManager.Instance.CurQuestIndex == 0)
+		if (QuestManager.Instance.CurQuestIndex == 1)
 		{
 			base.Temp();
-			QuestManager.Instance.QuestClearCheck(0);
+			QuestManager.Instance.QuestClearCheck(1);
 		}
 	}
 }
